66189dee0176f66aafa578a0384421967f69bf7b
yenyu <cyu2197@gmail.com>
2021-02-02 10:36:39 +0800

    Commit - 1612233397537

--- common/terraform/site/skt/staging/main.tf
+++ common/terraform/site/skt/staging/main.tf
@@ -113,7 +113,6 @@ resource "azurerm_network_security_rule" {
 
     // * Beijing
     "1.200.160.210/32",
-    "152.30.194.248/32",
     "101.230.75.2/32",
   ]
   source_application_security_group_ids = []
