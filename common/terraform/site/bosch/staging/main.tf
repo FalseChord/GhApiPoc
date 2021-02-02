e75f4101c99a025496629472ae84f6c8be32b69f
yenyu <cyu2197@gmail.com>
2021-02-02 11:52:23 +0800

    test in windows

--- common/terraform/site/bosch/staging/main.tf
+++ common/terraform/site/bosch/staging/main.tf
@@ -114,7 +114,6 @@ resource "azurerm_network_security_rule" {
     // * Beijing
     "1.200.160.210/32",
     "11.101.200.160/32",
-    "152.30.194.248/32",
     "101.230.75.2/32",
   ]
   source_application_security_group_ids = []
